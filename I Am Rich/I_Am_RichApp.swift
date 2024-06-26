4c64cf651e61b565bf29fbadf4e941afbf1b5f57
nateloading17 <nathanmilobaez@gmail.com>
2024-06-26 14:51:23 -0400

    Initial Commit

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Nathaniel Baez on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
